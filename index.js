6f64501bc04e3c7998ac7536ba64405302383e74
PPPDUD <107440101+PPPDUD@users.noreply.github.com>
2026-05-20 17:19:20 -0400

    Test out new formatter rules

--- index.js
+++ index.js
@@ -21,7 +21,6 @@
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
     SOFTWARE.
 */
-
 if (window.__crackle__) {
     throw new Error("Another instance of Sparkle is already running; quitting!");
 }
