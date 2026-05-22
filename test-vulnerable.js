5c5a12bc591da21bd413c775a65e9e5e3fabeb4a
PPPDUD <107440101+PPPDUD@users.noreply.github.com>
2026-05-22 11:41:48 -0400

    Test out CodeQL scanning

---
+++ test-vulnerable.js
@@ -0,0 +1,15 @@
+// Test file to see if CodeQL detects code smells properly.
+const app = require("express")(),
+      pg = require("pg"),
+      pool = new pg.Pool(config);
+
+app.get("search", function handler(req, res) {
+  // BAD: the category might have SQL special characters in it
+  var query1 =
+    "SELECT ITEM,PRICE FROM PRODUCT WHERE ITEM_CATEGORY='" +
+    req.params.category +
+    "' ORDER BY PRICE";
+  pool.query(query1, [], function(err, results) {
+    // process results
+  });
+});
