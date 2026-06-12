579e6e986ff973dd025c105b14164ffdea7d6498
PPPDUD <107440101+PPPDUD@users.noreply.github.com>
2026-06-12 11:33:13 -0400

    Consolidate comment lines

--- package.sh
+++ package.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 # Compress all of the files required for a working Sparkle installation into dist. Contains sparkle.zip (the code),
-# the raw sparkle.js, sparkle.min.js (minified version of sparkle.js), and sparkle.bookmarklet.js (sparkle.min.js
-# as a bookmarklet)
+# the raw sparkle.js, sparkle.min.js (minified version of sparkle.js), and sparkle.bookmarklet.js (sparkle.min.js as a bookmarklet)
 
 mkdir -p dist
 rm -f dist/sparkle.zip
