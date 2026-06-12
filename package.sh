33360eedbca039e2a6f03a463fc35962804a53cf
codingisfun2831t <124097137+codingisfun2831t@users.noreply.github.com>
2026-06-11 21:35:10 -0400

    improve package.js

--- package.sh
+++ package.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/bash
-# Compress all of the files required for a working Sparkle installation into a file named sparkle.zip. Requires node.js.
-mkdir -p dist/
-cd dist/
-rm -f sparkle.zip
-zip -9 sparkle.zip ../icons/* ../LICENSE ../index.js ../manifest.json
-cp ../index.js sparkle.js
-npx terser  sparkle.js -c -m -o sparkle.min.js
-printf "javascript:%s" "$(< sparkle.min.js)"  > sparkle.bookmarklet.js
+# Compress all of the files required for a working Sparkle installation into dist. Contains sparkle.zip (the code),
+# the raw sparkle.js, sparkle.min.js (minified version of sparkle.js), and sparkle.bookmarklet.js (sparkle.min.js
+# as a bookmarklet)
+
+mkdir -p dist
+rm -f dist/sparkle.zip
+zip -9 dist/sparkle.zip LICENSE index.js manifest.json icons/*
+cp index.js dist/sparkle.js
+npx terser dist/sparkle.js -c -m -o dist/sparkle.min.js
+printf 'javascript:' > dist/sparkle.bookmarklet.js
+cat dist/sparkle.min.js >> dist/sparkle.bookmarklet.js
