d2904695f18af76bf2ba5ac1822bcf90cb8403c3
PPPDUD <107440101+PPPDUD@users.noreply.github.com>
2026-06-13 12:14:25 -0400

    Backport ../ patch

--- package.sh
+++ package.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/sh
 # Compress all of the files required for a working Sparkle installation into a file named sparkle.zip.
-mkdir -p dist/
-cd dist/
-rm -f sparkle.zip
-zip -9 sparkle.zip ../icons/* ../LICENSE ../index.js ../manifest.json
-cp ../index.js sparkle.js
+mkdir -p dist
+rm -f dist/sparkle.zip
+zip -9 dist/sparkle.zip LICENSE index.js manifest.json icons/*
+cp index.js dist/sparkle.js
