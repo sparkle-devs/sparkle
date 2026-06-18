a8a998adce018f9f914b14cbadd3426a0ce915f2
PPPDUD <107440101+PPPDUD@users.noreply.github.com>
2026-06-18 18:56:37 -0400

    Bump version number (part 2)

--- index.js
+++ index.js
@@ -1547,7 +1547,7 @@ function preloadAddonFromPath(path) {
 
     // create the __crackle__ object
     window.__crackle__ = {
-        versionArray: [0, 10, 2]
+        versionArray: [0, 11, 0]
     };
     Object.assign(window.__crackle__, {
         version: API.versionStringFromSemver(window.__crackle__.versionArray),
